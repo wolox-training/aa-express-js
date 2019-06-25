7217504f2da8065adcb1f40335bc34c7e0376861
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-25 14:10:27 -0300

    Add eslint-disable new-cap

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -1,11 +1,14 @@
+/* eslint-disable new-cap */
+
 const express = require('express');
+
+const config = require('../../config').common.albums;
 
 const albumsRouter = express.Router();
 
-albumsRouter.get('/', (req, res, next) => {
-
+albumsRouter.get('/', (req, res) => {
+  const albums = fetch(config.api_url);
+  res.status(200).send(albums);
 });
 
-albumsRouter.get('/:id/photos', (req, res, next) => {
-    
-})
+module.exports = albumsRouter;
