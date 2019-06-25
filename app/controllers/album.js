06219fb6b0b971ffde61f86b9845eef8968c0f89
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-25 14:09:49 -0300

    Add route "/"

---
+++ app/controllers/album.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const albumsRouter = express.Router();
+
+albumsRouter.get('/', (req, res, next) => {
+
+});
+
+albumsRouter.get('/:id/photos', (req, res, next) => {
+    
+})
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,9 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
-const { albums } = require('./controllers/albums.js');
+const { albumsRouter } = require('./controllers/albums.js');
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.get('/albums', albums);
+  app.get('/albums', albumsRouter);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
