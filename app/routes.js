82d15cc54f51677a8fe6a5a694a776682cc86b2b
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-25 13:37:03 -0300

    Create album controller and add to routes

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,9 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
+const { albums } = require('./controllers/albums.js');
 exports.init = app => {
   app.get('/health', healthCheck);
+  app.get('/albums', albums);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
