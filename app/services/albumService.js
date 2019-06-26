e9d2f22681e8156f04e1c7e88ea89dc35f0ef089
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-26 10:28:08 -0300

    Write getPhotoOfAlbum

--- app/services/albumService.js
+++ app/services/albumService.js
@@ -5,7 +5,20 @@ const config = require('../../config').common.albums;
 module.exports = {
   getAlbums: async () => {
     try {
-      const response = await fetch(config.api_url);
+      const endpoint = `${config.api_url}/albums`;
+      const response = await fetch(endpoint);
+      if (response.status !== 200) {
+        throw new Error('Error calling api');
+      }
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  },
+  getPhotosOfAlbum: async albumId => {
+    const endpoint = `${config.api_url}/albums/${albumId}/photos`;
+    try {
+      const response = await fetch(endpoint);
       if (response.status !== 200) {
         throw new Error('Error calling api');
       }
