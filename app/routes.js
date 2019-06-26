28a8d357bdb20b13fd8a8f742a9401651b1c0ff7
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-26 11:24:19 -0300

    Refactor Routes

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,10 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
-const albumsRouter = require('./controllers/album.js');
+const albumsController = require('./controllers/album.js');
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.use('/albums', albumsRouter);
+  app.get('/albums', albumsController.getAlbums);
+  app.get('/albums/:id/photos', albumsController.getPhotoOfAlbum);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
