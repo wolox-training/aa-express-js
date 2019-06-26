6204d04f54b8830dc23b2898feb70d49f73683c6
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-26 09:10:21 -0300

    Write getAlgums function

--- app/services/albumService.js
+++ app/services/albumService.js
@@ -1,17 +1,12 @@
-const XHR = require(XMLHttpRequest);
-
 const config = require('../../config').common.albums;
 
 module.exports = {
-    getAlbums : () => {
-        const xhr = new XHR();
-        xhr.responseType = 'json';
-        xhr.onreadystatechange = () => {
-          if (xhr.readyState === XMLHttpRequest.DONE) {
-            return xhr.response
-          }
-        };
-        xhr.open('GET', config.api_url);
-        xhr.send();
+  getAlbums: async () => {
+    try {
+      const res = await fetch(config.api_url);
+      return res;
+    } catch (e) {
+      throw e;
     }
-}
+  }
+};
