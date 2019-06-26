1e7f23beff7b03e40288ed24d04766b13cd0b0e0
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-26 09:23:28 -0300

    Lintern Fixes

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -2,21 +2,23 @@
 
 const express = require('express');
 
-const config = require('../../config').common.albums;
 const albumService = require('../services/albumService');
 const log = require('../logger');
 
 const albumsRouter = express.Router();
 
-albumsRouter.get('/', (req, res) => {
-  albumService.getAlbums().then(albums => {
-    log.info(albums);
-    res.status(200).send(albums);
-  }).catch(err => {
-    log.error(err.message);
-    err.internalCode = "CONECTION_ERROR";
-    next(err);
-  });
+albumsRouter.get('/', (req, res, next) => {
+  albumService
+    .getAlbums()
+    .then(albums => {
+      log.info(albums);
+      res.status(200).send(albums);
+    })
+    .catch(err => {
+      log.error(err.message);
+      err.internalCode = 'CONECTION_ERROR';
+      next(err);
+    });
 });
 
 module.exports = albumsRouter;
