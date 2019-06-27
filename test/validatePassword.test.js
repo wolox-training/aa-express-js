916687566fc099fee417ce30e8c23d34faf6ed4d
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-27 12:13:57 -0300

    Add test for validate password

---
+++ test/validatePassword.test.js
@@ -0,0 +1,17 @@
+const { validatePassword } = require('../app/services/userService');
+
+const shortPass = '1234';
+const equalPass = '12345678';
+const longPass = '12345678912346579';
+
+test('No validate password shorter than 8 characters', () => {
+  expect(validatePassword(shortPass)).toBeFalsy();
+});
+
+test('Validate password equal to 8 characters', () => {
+  expect(validatePassword(equalPass)).toBeTruthy();
+});
+
+test('Validate password longer than 8 characters', () => {
+  expect(validatePassword(longPass)).toBeTruthy();
+});
