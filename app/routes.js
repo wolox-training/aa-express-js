4fcb693a634d676a87f3fe13557495c4c0b128a0
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-06-27 09:57:55 -0300

    Add route '/user'

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,12 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
 const albumsController = require('./controllers/album.js');
+const userController = require('./controllers/users');
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', albumsController.getAlbums);
   app.get('/albums/:id/photos', albumsController.getPhotoOfAlbum);
+  app.post('/users', userController.addUser);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
