04a013b3153a8bfc1812efa1ef14e223d7a0f724
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-01 10:39:39 -0300

    Add Middleware to route

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,12 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const albumsController = require('./controllers/album.js');
 const userController = require('./controllers/users');
+const userMiddle = require('./middlewares/userMiddle');
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', albumsController.getAlbums);
   app.get('/albums/:id/photos', albumsController.getPhotoOfAlbum);
-  app.post('/users', userController.addUser);
+  app.post('/users', [userMiddle.checkUserProperties, userMiddle.validatePassword], userController.addUser);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
