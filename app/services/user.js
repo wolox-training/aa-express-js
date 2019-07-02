6c61d8324a73ef3f5c3941c49aeb7d196a8f8b09
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-02 11:26:40 -0300

    Add email validation

--- app/services/user.js
+++ app/services/user.js
@@ -1,34 +1,39 @@
 const bcrypt = require('bcryptjs');
 
 const db = require('../models');
+const userConfig = require('../../config').common.user;
 const saltRounds = 10;
 
-module.exports = {
-  checkUserProperties: query => {
-    if (query.firstName && query.lastName && query.email && query.password) {
-      return true;
-    }
-    return false;
-  },
-  createUser: query => {
-    try {
-      return bcrypt.hash(query.password, saltRounds).then(async hash => {
-        const result = await db.User.create({
-          firstName: query.firstName,
-          lastName: query.lastName,
-          email: query.email,
-          password: hash
-        });
-        return result;
-      });
-    } catch (e) {
-      throw e;
-    }
-  },
-  validatePassword: password => {
-    if (password.length >= 8) {
-      return true;
-    }
-    return false;
+exports.checkUserProperties = query => {
+  if (query.firstName && query.lastName && query.email && query.password) {
+    return true;
   }
+  return false;
+};
+exports.createUser = query => {
+  try {
+    return bcrypt.hash(query.password, saltRounds).then(async hash => {
+      const result = await db.users.create({
+        firstName: query.firstName,
+        lastName: query.lastName,
+        email: query.email,
+        password: hash
+      });
+      return result;
+    });
+  } catch (e) {
+    throw e;
+  }
+};
+exports.validatePassword = password => {
+  if (RegExp(userConfig.password_regex).test(password)) {
+    return true;
+  }
+  return false;
+};
+exports.validateEmail = email => {
+  if (RegExp(userConfig.email_regex).test(email)) {
+    return true;
+  }
+  return false;
 };
