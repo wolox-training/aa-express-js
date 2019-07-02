7eca76d880252e3fcf1ab1cdfa10a2c25ca9add3
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-02 11:27:07 -0300

    Change module.exports for exportes

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,34 +1,37 @@
-const services = require('../services/userService');
+const services = require('../services/user');
 const log = require('../logger');
 
-module.exports = {
-  addUser: (req, res, next) => {
-    if (!services.checkUserProperties(req.query)) {
-      const err = new Error('User atributte missing');
-      err.internalCode = 'BAD_REQUEST';
-      next(err);
-    }
-    if (!services.validatePassword(req.query.password)) {
-      const err = new Error('Password is too short');
-      err.internalCode = 'BAD_REQUEST';
-      next(err);
-    }
-    services
-      .createUser(req.query)
-      .then(result => {
-        log.info(`User created first name: ${result.firstName}, last name: ${result.lastName}`);
-        return result;
-      })
-      .then(result =>
-        res
-          .json(result)
-          .status(201)
-          .send()
-      )
-      .catch(err => {
-        err.internalCode = 'DATABASE_ERROR';
-        log.error(err.message);
-        next(err);
-      });
+exports.addUser = (req, res, next) => {
+  if (!services.checkUserProperties(req.body)) {
+    const err = new Error('User atributte missing');
+    err.internalCode = 'bad_request';
+    return next(err);
   }
+  if (!services.validatePassword(req.body.password)) {
+    const err = new Error('Password is not valid');
+    err.internalCode = 'bad_request';
+    return next(err);
+  }
+  if (!services.validateEmail(req.body.email)) {
+    const err = new Error('Email is not valid');
+    err.internalCode = 'bad_request';
+    return next(err);
+  }
+  return services
+    .createUser(req.body)
+    .then(result => {
+      log.info(`User created first name: ${result.firstName}, last name: ${result.lastName}`);
+      return result;
+    })
+    .then(result =>
+      res
+        .json(result)
+        .status(201)
+        .send()
+    )
+    .catch(err => {
+      err.internalCode = 'DATABASE_ERROR';
+      log.error(err.message);
+      return next(err);
+    });
 };
