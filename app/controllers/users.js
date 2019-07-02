4241f8695bb77cdd2f6493793de576410a07f913
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-02 12:20:39 -0300

    Refactor user conrtroller and user service

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,23 +1,8 @@
 const services = require('../services/user');
 const log = require('../logger');
 
-exports.addUser = (req, res, next) => {
-  if (!services.checkUserProperties(req.body)) {
-    const err = new Error('User atributte missing');
-    err.internalCode = 'bad_request';
-    return next(err);
-  }
-  if (!services.validatePassword(req.body.password)) {
-    const err = new Error('Password is not valid');
-    err.internalCode = 'bad_request';
-    return next(err);
-  }
-  if (!services.validateEmail(req.body.email)) {
-    const err = new Error('Email is not valid');
-    err.internalCode = 'bad_request';
-    return next(err);
-  }
-  return services
+exports.addUser = (req, res, next) =>
+  services
     .createUser(req.body)
     .then(result => {
       log.info(`User created first name: ${result.firstName}, last name: ${result.lastName}`);
@@ -30,8 +15,6 @@ exports.addUser = (req, res, next) => {
         .send()
     )
     .catch(err => {
-      err.internalCode = 'DATABASE_ERROR';
       log.error(err.message);
       return next(err);
     });
-};
