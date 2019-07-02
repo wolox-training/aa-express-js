d4958d55ddb7ae6d31439e1512ba9c52466f3f04
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-02 09:14:39 -0300

    Change module.export to exports in the controller

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -1,32 +1,30 @@
 const albumService = require('../services/album');
 const log = require('../logger');
 
-module.exports = {
-  getAlbums: (req, res, next) => {
-    albumService
-      .getAlbums()
-      .then(response => response.json())
-      .then(albums => {
-        res.status(200).send(albums);
-      })
-      .catch(err => {
-        log.error(err.message);
-        err.internalCode = 'CONECTION_ERROR';
-        next(err);
-      });
-  },
+exports.getAlbums = (req, res, next) => {
+  albumService
+    .getAlbums()
+    .then(response => response.json())
+    .then(albums => {
+      res.status(200).send(albums);
+    })
+    .catch(err => {
+      log.error(err.message);
+      err.internalCode = 'CONECTION_ERROR';
+      next(err);
+    });
+};
 
-  getPhotoOfAlbum: (req, res, next) => {
-    const albumId = req.params.id;
-    albumService
-      .getPhotosOfAlbum(albumId)
-      .then(photos => {
-        res.status(200).send(photos);
-      })
-      .catch(err => {
-        log.error(err.message);
-        err.internalCode = 'CONECTION_ERROR';
-        next(err);
-      });
-  }
+exports.getPhotoOfAlbum = (req, res, next) => {
+  const albumId = req.params.id;
+  albumService
+    .getPhotosOfAlbum(albumId)
+    .then(photos => {
+      res.status(200).send(photos);
+    })
+    .catch(err => {
+      log.error(err.message);
+      err.internalCode = 'CONECTION_ERROR';
+      next(err);
+    });
 };
