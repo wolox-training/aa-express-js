03b6f2e1ff9e230bd14bb5844958379e2efd06b5
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-03 10:34:55 -0300

    Add logInt Tests

---
+++ test/signInEndpoint.test.js
@@ -0,0 +1,61 @@
+const request = require('supertest');
+const Chance = require('chance');
+
+const app = require('../app');
+
+const chance = new Chance();
+
+const firstRightQuery = {
+  firstName: chance.first(),
+  lastName: chance.last(),
+  email: chance.email({ domain: 'wolox.com.ar' }),
+  password: chance.string({ length: 10, pool: 'asdfghjkl147258369' })
+};
+
+const logInRightQuery = {
+  email: firstRightQuery.email,
+  password: firstRightQuery.password
+};
+
+const logInWrongEmailQuery = {
+  email: chance.email({ domain: 'wolox.com.ar' }),
+  password: firstRightQuery.password
+};
+
+const logInWrongPassQuery = {
+  email: firstRightQuery.email,
+  password: chance.string({ length: 10, pool: 'asdfghjkl147258369' })
+};
+
+test('Correct Log In', async () => {
+  await request(app)
+    .post('/users')
+    .send(firstRightQuery)
+    .expect(200);
+  await request(app)
+    .post('/users/sessions')
+    .send(logInRightQuery)
+    .expect(200);
+});
+
+test('Wrong Email Log In', async () => {
+  await request(app)
+    .post('/users')
+    .send(firstRightQuery)
+    .expect(200);
+  await request(app)
+    .post('/users/sessions')
+    .send(logInWrongEmailQuery)
+    .expect(400);
+});
+
+test('Wrong Password Log In', async () => {
+  await request(app)
+    .post('/users')
+    .send(firstRightQuery)
+    .expect(200);
+  await request(app)
+    .post('/users/sessions')
+    .send(logInWrongPassQuery)
+    .expect(400);
+});
