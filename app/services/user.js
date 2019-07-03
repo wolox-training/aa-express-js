fb95d9008faae08cffc56275e4cd7aa39b974fe1
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-03 09:57:21 -0300

    Refactor user services

--- app/services/user.js
+++ app/services/user.js
@@ -1,31 +1,9 @@
 const bcrypt = require('bcryptjs');
 
 const db = require('../models');
-const userConfig = require('../../config').common.user;
 const saltRounds = 10;
 
-exports.checkUserProperties = query => {
-  if (query.firstName && query.lastName && query.email && query.password) {
-    return true;
-  }
-  return false;
-};
 exports.createUser = query => {
-  if (!this.checkUserProperties(query)) {
-    const err = new Error('User atributte missing');
-    err.internalCode = 'bad_request';
-    throw err;
-  }
-  if (!this.validatePassword(query.password)) {
-    const err = new Error('Password is not valid');
-    err.internalCode = 'bad_request';
-    throw err;
-  }
-  if (!this.validateEmail(query.email)) {
-    const err = new Error('Email is not valid');
-    err.internalCode = 'bad_request';
-    throw err;
-  }
   try {
     return bcrypt.hash(query.password, saltRounds).then(async hash => {
       try {
@@ -49,16 +27,4 @@ exports.createUser = query => {
     e.internalCode = 'database_error';
     throw e;
   }
-};
-exports.validatePassword = password => {
-  if (RegExp(userConfig.password_regex).test(password)) {
-    return true;
-  }
-  return false;
-};
-exports.validateEmail = email => {
-  if (RegExp(userConfig.email_regex).test(email)) {
-    return true;
-  }
-  return false;
 };
