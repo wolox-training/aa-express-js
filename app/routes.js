64f6de38065ad15dbb00ba3ea91f626246cef56f
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-03 13:00:08 -0300

    Add get user route

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ const { healthCheck } = require('./controllers/healthCheck');
 const albumsController = require('./controllers/album.js');
 const userController = require('./controllers/users');
 const userMiddle = require('./middlewares/user');
+const securityMiddle = require('./middlewares/security');
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', albumsController.getAlbums);
@@ -17,6 +18,7 @@ exports.init = app => {
     [userMiddle.validateEmail, userMiddle.validatePassword],
     userController.loginUser
   );
+  app.get('/users', [securityMiddle.checkToken], userController.getUsers);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
