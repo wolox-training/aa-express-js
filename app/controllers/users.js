290767c554e579a5026b16c443a7d0868d802950
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-12 09:10:41 -0300

    Remove returning of user created

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,19 +1,16 @@
-const services = require('../services/user');
+const usersServices = require('../services/user');
 const log = require('../logger');
 
 exports.addUser = (req, res, next) =>
-  services
+  usersServices
     .createUser(req.body)
     .then(result => {
       log.info(`User created first name: ${result.firstName}, last name: ${result.lastName}`);
       return result;
     })
-    .then(result =>
-      res
-        .json(result)
-        .status(201)
-        .send()
-    )
+    .then(() => {
+      res.status(201).send();
+    })
     .catch(err => {
       log.error(err.message);
       return next(err);
