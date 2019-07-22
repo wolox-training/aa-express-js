d75d4f8b22a2499d4b513329d123ab9a01cde1ad
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-22 10:29:15 -0300

    Use express validator

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,8 +1,14 @@
 const usersService = require('../services/user');
 const log = require('../logger');
+const { validationResult } = require('express-validator');
+const errorsDefinition = require('../errors');
 
-exports.addUser = (req, res, next) =>
-  usersService
+exports.addUser = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    next(errorsDefinition.badRequest(errors.array()[0].msg));
+  }
+  return usersService
     .createUser(req.body)
     .then(result => {
       log.info(`User created first name: ${result.firstName}, last name: ${result.lastName}`);
@@ -15,3 +21,4 @@ exports.addUser = (req, res, next) =>
       log.error(err.message);
       return next(err);
     });
+};
