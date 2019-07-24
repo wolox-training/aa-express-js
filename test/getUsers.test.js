7aeed25acc77a579c8d2845909973032eb04a3de
alejoacevedo <ale.acevedo@live.com.ar>
2019-07-24 12:09:57 -0300

    Fixing tests

--- test/getUsers.test.js
+++ test/getUsers.test.js
@@ -38,113 +38,115 @@ const fourRightQuery = {
   password: chance.string({ length: 10, pool: 'asdfghjkl147258369' })
 };
 
-test('Correct Get Users', async () => {
-  await request(app)
-    .post('/users')
-    .send(firstRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(secondRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(thirdRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(fourRightQuery)
-    .expect(200);
-  const res = await request(app)
-    .post('/users/sessions')
-    .send(logInRightQuery)
-    .expect(200);
-  let users = await request(app)
-    .get('/users')
-    .set('token', res.body.token)
-    .query({ page: '0', size: '1' })
-    .expect(200);
-  expect(users.body.length).toBe(1);
-  expect(users.body[0].email).toBe(firstRightQuery.email);
-  users = await request(app)
-    .get('/users')
-    .set('token', res.body.token)
-    .query({ page: '1', size: '1' })
-    .expect(200);
-  expect(users.body.length).toBe(1);
-  expect(users.body[0].email).toBe(secondRightQuery.email);
-  users = await request(app)
-    .get('/users')
-    .set('token', res.body.token)
-    .query({ page: '2', size: '1' })
-    .expect(200);
-  expect(users.body.length).toBe(1);
-  expect(users.body[0].email).toBe(thirdRightQuery.email);
-  users = await request(app)
-    .get('/users')
-    .set('token', res.body.token)
-    .query({ page: '3', size: '1' })
-    .expect(200);
-  expect(users.body.length).toBe(1);
-  expect(users.body[0].email).toBe(fourRightQuery.email);
-  users = await request(app)
-    .get('/users')
-    .set('token', res.body.token)
-    .query({ page: '0', size: '3' })
-    .expect(200);
-  expect(users.body.length).toBe(3);
-  expect(users.body[1].email).toBe(secondRightQuery.email);
-});
-test('Not Valid Token Get Users', async () => {
-  await request(app)
-    .post('/users')
-    .send(firstRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(secondRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(thirdRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(fourRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users/sessions')
-    .send(logInRightQuery)
-    .expect(200);
-  await request(app)
-    .get('/users')
-    .set('token', 'res.body.token')
-    .query({ page: '0', size: '1' })
-    .expect(403);
-});
-test('Not Token Get Users', async () => {
-  await request(app)
-    .post('/users')
-    .send(firstRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(secondRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(thirdRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users')
-    .send(fourRightQuery)
-    .expect(200);
-  await request(app)
-    .post('/users/sessions')
-    .send(logInRightQuery)
-    .expect(200);
-  await request(app)
-    .get('/users')
-    .query({ page: '0', size: '1' })
-    .expect(400);
+describe('Test get users', () => {
+  test('Correct Get Users', async () => {
+    await request(app)
+      .post('/users')
+      .send(firstRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(secondRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(thirdRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(fourRightQuery)
+      .expect(201);
+    const res = await request(app)
+      .post('/users/sessions')
+      .send(logInRightQuery)
+      .expect(200);
+    let users = await request(app)
+      .get('/users')
+      .set('token', res.body.token)
+      .query({ page: '0', size: '1' })
+      .expect(200);
+    expect(users.body.length).toBe(1);
+    expect(users.body[0].email).toBe(firstRightQuery.email);
+    users = await request(app)
+      .get('/users')
+      .set('token', res.body.token)
+      .query({ page: '1', size: '1' })
+      .expect(200);
+    expect(users.body.length).toBe(1);
+    expect(users.body[0].email).toBe(secondRightQuery.email);
+    users = await request(app)
+      .get('/users')
+      .set('token', res.body.token)
+      .query({ page: '2', size: '1' })
+      .expect(200);
+    expect(users.body.length).toBe(1);
+    expect(users.body[0].email).toBe(thirdRightQuery.email);
+    users = await request(app)
+      .get('/users')
+      .set('token', res.body.token)
+      .query({ page: '3', size: '1' })
+      .expect(200);
+    expect(users.body.length).toBe(1);
+    expect(users.body[0].email).toBe(fourRightQuery.email);
+    users = await request(app)
+      .get('/users')
+      .set('token', res.body.token)
+      .query({ page: '0', size: '3' })
+      .expect(200);
+    expect(users.body.length).toBe(3);
+    expect(users.body[1].email).toBe(secondRightQuery.email);
+  });
+  test('Not Valid Token Get Users', async () => {
+    await request(app)
+      .post('/users')
+      .send(firstRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(secondRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(thirdRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(fourRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users/sessions')
+      .send(logInRightQuery)
+      .expect(200);
+    await request(app)
+      .get('/users')
+      .set('token', 'res.body.token')
+      .query({ page: '0', size: '1' })
+      .expect(403);
+  });
+  test('Not Token Get Users', async () => {
+    await request(app)
+      .post('/users')
+      .send(firstRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(secondRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(thirdRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users')
+      .send(fourRightQuery)
+      .expect(201);
+    await request(app)
+      .post('/users/sessions')
+      .send(logInRightQuery)
+      .expect(200);
+    await request(app)
+      .get('/users')
+      .query({ page: '0', size: '1' })
+      .expect(400);
+  });
 });
