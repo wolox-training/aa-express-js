9ae03fba3aa4d7fd6750ce74d0d1dc6089728421
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-07-31 14:05:41 -0300

    Check user exist

--- app/services/security.js
+++ app/services/security.js
@@ -1,11 +1,15 @@
 const moment = require('moment');
 
 const User = require('../models').users;
+const errors = require('../errors');
 
 exports.validateToken = async tokenDecoded => {
   const user = await User.findOne({ where: { email: tokenDecoded.email } });
-  if (!user.timestampTokenCreation) {
-    return false;
+  if (!user) {
+    throw errors.notFound('User not exist');
   }
-  return moment(user.timestampTokenCreation).isSameOrBefore(moment(tokenDecoded.tokenCreation).format());
+  return (
+    user.timestampTokenCreation &&
+    moment(user.timestampTokenCreation).isSameOrBefore(moment(tokenDecoded.tokenCreation))
+  );
 };
